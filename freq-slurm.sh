29c92162247e9e2fa247454c81202dde2d00844b
BenjoBurger <90093905+BenjoBurger@users.noreply.github.com>
2025-09-29 00:41:26 +0800

    Remove merge conflict markers and add time print

--- freq-slurm.sh
+++ freq-slurm.sh
@@ -14,10 +14,6 @@
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 python -u -c "import PyHipp as pyh; \
-<<<<<<< HEAD
-pyh.FreqSpectrum(saveLevel=1); \
-pyh.FreqSpectrum(loadHighPass=True, pointsPerWindow=3000, saveLevel=1);
-=======
 import time; \
 pyh.FreqSpectrum(saveLevel=1); \
 pyh.FreqSpectrum(loadHighPass=True, pointsPerWindow=3000, saveLevel=1); \
